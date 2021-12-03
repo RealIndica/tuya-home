01c8ac94527612d59fe410f6901d0d8840e8001c
Leon Jordan <leonluke01@hotmail.co.uk>
2021-12-03 17:32:00 +0000

    Delete Tuya Home/Tuya Home/bin directory

--- Tuya Home/Tuya Home/bin/x64/Debug/bin/CLI/lib/helpers.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	regionToUrl: region => `https://openapi.tuya${region}.com`
-};
--- Tuya Home/Tuya Home/bin/x64/Debug/bin/arpPopulate.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set iptarget=%1
-for /L %%N in (1,1,254) do start /b ping -n 1 -w 200 %iptarget%.%%N
